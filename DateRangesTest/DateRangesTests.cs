e6abc3561d100a390e821a74f27cb00f250528e4
warlord89 <kamil.duczakowski@futurelife.com.pl>
2018-06-06 15:19:52 +0200

    tests project

---
+++ DateRangesTest/DateRangesTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateRanges;
+
+namespace DateRangesTest
+{
+    [TestClass]
+    public class DateRangesTests
+    {
+        [TestMethod]
+        public void MaxDateCheck()
+        {
+            DateTime d1 = DateTime.Parse("2018, 03, 04");
+            DateTime d2 = DateTime.Parse("2018, 03, 05");
+            DateTimeRange maximDate = new DateTimeRange(d1, d2); 
+            
+            Assert.AreEqual(DateTime.Parse("2018, 03, 05"), maximDate.MaxDate(d1,d2));
+        }
+    }
+}
---
+++ DateRangesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DateRangesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DateRangesTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00f7e44d-f049-4bc5-9a10-99dfd1452976")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
